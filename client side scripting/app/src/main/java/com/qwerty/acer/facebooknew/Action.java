ec902bf7ac3507af291058eaa6155e9071dc9c3a
abdulrauf1996 <abdulrauft1996@gmail.com>
2016-09-05 00:34:47 -0700

    facebook login is created

---
+++ client side scripting/app/src/main/java/com/qwerty/acer/facebooknew/Action.java
@@ -0,0 +1,14 @@
+package com.qwerty.acer.facebooknew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Action extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action);
+    }
+}
---
+++ client side scripting/app/src/main/java/com/qwerty/acer/facebooknew/Main2Activity.java
@@ -0,0 +1,24 @@
+package com.qwerty.acer.facebooknew;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+
+      // getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        //getSupportActionBar().setDisplayShowCustomEnabled(true);
+        //getSupportActionBar().setCustomView(R.layout.activity_action);
+
+        //View view=getSupportActionBar().getCustomView();
+
+    }
+}
